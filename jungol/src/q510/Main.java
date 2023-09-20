dfe88e467cf6323fe14aee079d69519a7fd5e9ff
heeja <heeja@Z-GRAM>
2023-09-20 13:36:38 +0900

    x 변수값 변경

--- jungol/src/q510/Main.java
+++ jungol/src/q510/Main.java
@@ -2,8 +2,13 @@ package q510;
 
 public class Main {
 	public static void main(String[] args) {
-		int x = -1;
-		int y = 100;
+		int x, y;
+		x = -1;
+		y = 100;
+		System.out.println(x);
+		System.out.println(y);
+		
+		x = 99;
 		System.out.println(x);
 		System.out.println(y);
 	}
