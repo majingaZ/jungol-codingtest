34ba7492a279fdadd569a98a204acfc647c3b6cb
heeja <heejaewg@gmail.com>
2023-10-11 21:15:48 +0900

    1011

--- jungol/src/q9071/Main.java
+++ jungol/src/q9071/Main.java
@@ -10,43 +10,19 @@ public class Main {
 			inp[i] = sc.nextInt();
 		}
 		sc.close();
-		
+
 		int max = -10000, min = 10000;
-		for (int i=0; i<inp.length; i++) {
-			if(inp[i] % 2 == 0) {
-				if (max < inp[i]) {
+		for (int i = 0; i < inp.length; i++) {
+			if (inp[i] % 2 == 0) {
+				if (inp[i] > max) {
 					max = inp[i];
-				} else {
-					if (min > inp[i]) {
-						min = inp[i];
-					}
+				}
+			} else {
+				if (inp[i] < min) {
+					min = inp[i];
 				}
 			}
 		}
-			System.out.println(min + " " + max);
-
-//		int[] a = new int[10];
-//		int[] b = new int[10];
-//
-//		for (int i = 0; i < inp.length; i++) {
-//			inp[i] = sc.nextInt();
-//		}
-//		sc.close();
-//        
-//		int m1 = Integer.MAX_VALUE;
-//		int m2 = Integer.MIN_VALUE;
-//		
-//		for (int i = 0; i < inp.length; i++) {
-//			if (inp[i] % 2 == 0) {
-//				a[i] += inp[i];
-//				m1 = Math.max(m1, inp[i]);
-//			} else {
-//				b[i] += inp[i];
-//				m2 = Math.min(m2, inp[i]);
-//			}
-//		}
-//
-//		System.out.println(m1);
-//		System.out.println(m2);
+		System.out.println(min + " " + max);
 	}
 }
